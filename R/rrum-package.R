f579fa7787bc3dd37b322aeb4473cca0b4afc37d
James J Balamuta <james.balamuta@gmail.com>
2017-09-29 01:06:43 -0500

    Add rrum package documentation

---
+++ R/rrum-package.R
@@ -0,0 +1,6 @@
+#' @useDynLib rrum, .registration=TRUE
+#' @importFrom Rcpp evalCpp
+#' @details 
+#' Implemention of the reduced Reparametrized Unified Model
+#' @template rrum-references
+"_PACKAGE"
