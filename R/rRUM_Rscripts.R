ef253cb97fbecdbbd386a4ef8471fcdd3c56a8fe
James J Balamuta <james.balamuta@gmail.com>
2017-11-30 14:54:25 -0600

    pi_reference shuffle

--- R/rRUM_Rscripts.R
+++ R/rRUM_Rscripts.R
@@ -54,21 +54,7 @@ rrum = function(Y, Q, chain_length = 10000L,
   rrum_helper(Y, Q, delta0, chain_length, as, bs, ag, bg)
 }
 
-#' Mapping of Entries of Pi to Latent Attribute Classes
-#'
-#' Obtains mapping of entries of pi to latent attribute classes.
-#' @param K A `numeric`, denoting the number of attributes.
-#' @return A `matrix`, where row c represents the latent class
-#' corresponding to entry c of pi
-#' @author Steven Andrew Culpepper, Aaron Hudson
+#' @inherit simcdm::pi_reference
+#' @importFrom simcdm pi_reference
 #' @export
-#' @template rrum-example
-#' @template rrum-references
-pi_reference = function(K) {
-  biject.vector = bijectionvector(K)
-  As = as.matrix(expand.grid(rep(list(c(0,1)), K)))
-  a = As%*%biject.vector
-  As = As[a+1,]
-  return(As)
-}
-
+pi_reference = simcdm::pi_reference
