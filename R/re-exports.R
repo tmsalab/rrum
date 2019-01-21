2b2b064c65ecc234709e9ff020a44e57b8f9442a
James Balamuta <balamut2@illinois.edu>
2019-01-20 19:14:47 -0600

    Add another re-export.

--- R/re-exports.R
+++ R/re-exports.R
@@ -7,3 +7,7 @@ simcdm::sim_attribute_classes
 #' @importFrom simcdm attribute_bijection
 #' @export
 simcdm::attribute_bijection
+
+#' @importFrom simcdm sim_rrum_items
+#' @export
+simcdm::sim_rrum_items
