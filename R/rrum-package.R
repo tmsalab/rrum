a3495ed8054ee0968fb061b26389477e66eb9857
James Balamuta <balamut2@illinois.edu>
2019-01-30 16:59:22 -0600

    Fix naming of attribute_classes()

--- R/rrum-package.R
+++ R/rrum-package.R
@@ -1,6 +1,6 @@
 #' @useDynLib rrum, .registration=TRUE
 #' @importFrom Rcpp evalCpp
-#' @importFrom simcdm attribute_bijection sim_attribute_classes sim_rrum_items
+#' @importFrom simcdm attribute_bijection attribute_classes sim_rrum_items
 #' @details 
 #' Implemention of a bayesian estimation for the 
 #' reduced Reparametrized Unified Model (rRUM).
