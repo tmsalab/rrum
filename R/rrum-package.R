10583d07cf641e122979857638dd527bad544906
James Balamuta <balamut2@illinois.edu>
2020-07-31 11:21:04 -0500

    Re-build package documentation

--- R/rrum-package.R
+++ R/rrum-package.R
@@ -1,9 +1,16 @@
-#' @useDynLib rrum, .registration=TRUE
-#' @importFrom Rcpp evalCpp
-#' @importFrom simcdm attribute_bijection attribute_classes sim_rrum_items
 #' @details 
 #' Implemention of a Bayesian estimation for the 
 #' reduced Reparameterized Unified Model (rRUM).
 #' @template rrum-references
 #' @aliases rrum-package
+#' @keywords internal
 "_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @useDynLib rrum, .registration=TRUE
+#' @importFrom Rcpp evalCpp
+#' @importFrom simcdm attribute_bijection attribute_classes sim_rrum_items
+## usethis namespace: end
+NULL
